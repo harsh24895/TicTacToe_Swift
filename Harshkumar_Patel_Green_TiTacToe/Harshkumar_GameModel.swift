acf43f2e19ed2cf5b6df2cdb53fbe6439bdd1872
Harshkumar Patel <200394746@ba-a251-2328.student.georgianc.on.ca>
2019-10-09 17:11:45 -0400

    half done

--- Harshkumar_Patel_Green_TiTacToe/Harshkumar_GameModel.swift
+++ Harshkumar_Patel_Green_TiTacToe/Harshkumar_GameModel.swift
@@ -10,6 +10,9 @@ import Foundation
 
 class Harshkumar_GameModel{
     var whoseTurn="X"
+    var numberOfMovesplayed = 0
+    var lastPlayed = ""
+    var whoWon = ""
     var squareContents = Array(repeating: "", count: 10)
     let winnigCombination=[
     [1,2,3],
@@ -20,4 +23,36 @@ class Harshkumar_GameModel{
     [3,6,9],
     [1,5,9],
     [3,5,7]]
+    
+    func playMove(tag: Int) {
+        squareContents[tag] = whoseTurn
+        lastPlayed = whoseTurn
+        if(whoseTurn == "X") {
+            whoseTurn = "0"
+        }else{
+            whoseTurn = "X"
+        }
+        numberOfMovesplayed += 1
+    }
+    
+    func isGamefinished() -> Bool{
+        if(numberOfMovesplayed < 5){
+            return false
+        }else if(numberOfMovesplayed == 9){
+            return true
+        }
+        for winningCombo in winnigCombination{
+            let index1 = winningCombo[0]
+            let index2 = winningCombo[1]
+            let index3 = winningCombo[2]
+            
+            if (squareContents[index1] == lastPlayed && squareContents[index2] == lastPlayed && squareContents[index3] == lastPlayed){
+                whoWon = lastPlayed
+                return true
+            }
+
+        }
+        
+        return false
+    }
 }
--- Harshkumar_Patel_Green_TiTacToe/Harshkumar_ViewController.swift
+++ Harshkumar_Patel_Green_TiTacToe/Harshkumar_ViewController.swift
@@ -9,14 +9,42 @@
 import UIKit
 
 class Harshkumar_ViewController: UIViewController {
+    
+    var gamemodel = Harshkumar_GameModel()
+    
+    //its mark for outlet
+    @IBOutlet weak var gameStatelabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+            gameStatelabel.text = gamemodel.whoseTurn + "Turn"
         // Do any additional setup after loading the view.
     }
     
 
+    @IBAction func squareTouched(_ sender: UIButton) {
+        print("Touched")
+        print(sender.tag)
+        if(sender.currentTitle == "X"||sender.currentTitle == "O"){
+            
+        }else{
+            sender.setTitle(gamemodel.whoseTurn, for: .normal)
+            gamemodel.playMove(tag: sender.tag)
+            //check if the game is finished
+            let gameFinished = gamemodel.isGamefinished()
+            
+            if(gameFinished){
+                let whoWon = gamemodel.whoWon
+                gameStatelabel.text = whoWon + "Won"
+                print(whoWon)
+            }else{
+                gameStatelabel.text = gamemodel.whoseTurn + "'sTurn"
+            }
+            
+
+        }
+    }
     /*
     // MARK: - Navigation
 
